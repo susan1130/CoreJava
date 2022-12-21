0ccd116df0ed6ecc5153cb7dbf8608739d797788
SusanDas <48977015+susan1130@users.noreply.github.com>
2022-12-21 19:25:24 +0530

    Add files via upload

---
+++ 02_WrapperClassBasics/src/com/dl/one/Eg8.java
@@ -0,0 +1,20 @@
+package com.dl.one;
+
+public class Eg8 {
+
+	public static void main(String[] args) {
+		int i = 100;
+		Integer i1 = i;
+		System.out.println(i1);
+		
+		int i2 = 20;
+		Long l1 =(long) i2;
+		System.out.println(l1);
+		
+		Byte b = 10;
+		Short sh = (short)b;
+		System.out.println(sh);
+
+	}
+
+}
